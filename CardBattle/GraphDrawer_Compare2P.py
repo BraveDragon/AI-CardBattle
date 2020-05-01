fe93d936ce33cb904817ba65ac44443e89e1ccad
BraveDragon <masa010515@hera.eonet.ne.jp>
2020-05-01 17:36:22 +0900

    グラフを追加

--- CardBattle/GraphDrawer_Compare2P.py
+++ CardBattle/GraphDrawer_Compare2P.py
@@ -4,21 +4,20 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import pandas as pd
 
-Data1P = pd.read_csv("Model_EZero\Model1PRating.csv",header=None)
 Data2P = pd.read_csv("Model_EZero\Model2PRating.csv",header=None)
+Data2P_DQN = pd.read_csv("Model\DQNModel2PRating.csv",header=None)
 
 
-x1P = Data1P[0]
-y1P = Data1P[1]
 x2P = Data2P[0]
 y2P = Data2P[1]
-
+x2P_DQN = Data2P_DQN[0]
+y2P_DQN = Data2P_DQN[1]
 
 plt.xlabel("Games")
 plt.ylabel("Rating")
-plt.plot(x1P,y1P,label="1P")
 plt.plot(x2P,y2P,label="2P")
+plt.plot(x2P_DQN,y2P_DQN,label="2P_DQN")
 plt.legend()
 plt.show()
-plt.savefig("Graph.png")
+plt.savefig("Graph_Compare2P.png")
 
--- CardBattle/GraphDrawer_DQN.py
+++ CardBattle/GraphDrawer_DQN.py
@@ -2,7 +2,6 @@
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 Data1P_DQN = pd.read_csv("Model\DQNModel1PRating.csv",header=None)
