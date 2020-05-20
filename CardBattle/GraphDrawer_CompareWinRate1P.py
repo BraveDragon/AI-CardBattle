ee889a8c79f54cf30a894e171e1b84db7b1ac602
BraveDragon <masa010515@hera.eonet.ne.jp>
2020-05-20 18:03:48 +0900

    結果のグラフを追加

---
+++ CardBattle/GraphDrawer_CompareWinRate1P.py
@@ -0,0 +1,23 @@
+#coding: "utf-8"
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import pandas as pd
+
+Data1P = pd.read_csv("Model_EZero\EZeroWinRate.csv",header=None)
+Data1P_DQN = pd.read_csv("Model\DQNWinRate.csv",header=None)
+
+
+x1P = Data1P[0]
+y1P = Data1P[1]
+x1P_DQN = Data1P_DQN[0]
+y1P_DQN = Data1P_DQN[1]
+
+plt.xlabel("Games")
+plt.ylabel("Rating")
+plt.plot(x1P,y1P,label="1P")
+plt.plot(x1P_DQN,y1P_DQN,label="1P_DQN")
+plt.legend()
+plt.show()
+plt.savefig("Graph_CompareWinRate1P.png")
+
---
+++ CardBattle/GraphDrawer_CompareWinRate2P.py
@@ -0,0 +1,23 @@
+#coding: "utf-8"
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import pandas as pd
+
+Data2P = pd.read_csv("Model_EZero\EZeroWinRate_2P.csv",header=None)
+Data2P_DQN = pd.read_csv("Model\DQNWinRate_2P.csv",header=None)
+
+
+x2P = Data2P[0]
+y2P = Data2P[1]
+x2P_DQN = Data2P_DQN[0]
+y2P_DQN = Data2P_DQN[1]
+
+plt.xlabel("Games")
+plt.ylabel("Rating")
+plt.plot(x2P,y2P,label="2P")
+plt.plot(x2P_DQN,y2P_DQN,label="2P_DQN")
+plt.legend()
+plt.show()
+plt.savefig("Graph_CompareWinRate2P.png")
+
