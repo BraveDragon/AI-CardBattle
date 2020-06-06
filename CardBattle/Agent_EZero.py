ac77820729a9a6ec3c4259ec070f3d7b68a25a92
Bravedragon <masa010515@hera.eonet.ne.jp>
2020-06-06 17:27:26 +0900

    エラー修正

--- CardBattle/Agent_EZero.py
+++ CardBattle/Agent_EZero.py
@@ -9,8 +9,6 @@ import numpy as np
 from collections import deque
 import random
 import EZeroModel
-#ReplayMemoryのデータをUnityと共有するために使用
-#import json
 #ELOレーティング用のライブラリ(MITライセンスなので多分大丈夫だがライセンスは要確認)
 from elote import EloCompetitor
 
@@ -53,7 +51,8 @@ Winning_2P = 0
 Model1P_Target = EZeroModel.Model.to(Devise)
 Model2P_Target = EZeroModel.Model.to(Devise)
 #ここからが実際の学習のコード
-optimizer = optim.Adam(EZeroModel.Model.parameters(),lr=0.001,weight_decay=0.005)
+optimizer1P = optim.Adam(EZeroModel.Model.parameters(),lr=0.001,weight_decay=0.005)
+optimizer2P = optim.Adam(EZeroModel.Model.parameters(),lr=0.001,weight_decay=0.005)
 MaxSteps = 500
 episodes = 10000
 CurrentStep = 0
@@ -88,10 +87,12 @@ for episode in range(episodes+1):
         out2P = Model2P_Target(torch.from_numpy(np.random.rand(EZeroModel.Inputs)).to(Devise).float())
         loss1P = criterion(out1P,target1P)
         loss2P = criterion(out2P,target2P)
-        optimizer.zero_grad()
+        optimizer1P.zero_grad()
+        optimizer2P.zero_grad()
         loss1P.backward(retain_graph=True)
         loss2P.backward(retain_graph=True)
-        optimizer.step()
+        optimizer1P.step()
+        optimizer2P.step()
     else:
         Model1P_Target.load_state_dict(Model1P.state_dict())
         Model2P_Target.load_state_dict(Model2P.state_dict())
@@ -154,7 +155,7 @@ for episode in range(episodes+1):
                 else:
                     Load_Inputs1P.append(Input_itr)
             Load_Inputs1P = np.array(Load_Inputs1P,dtype=np.float32)
-            ret_action1P = Model1P(torch.from_numpy(Load_Inputs1P))
+            ret_action1P = Model1P(torch.from_numpy(Load_Inputs1P).to(Devise).float())
 
             Raw_Input2P = Memory_2P.sample(batch_size)
             Inputs2P = []
@@ -168,8 +169,9 @@ for episode in range(episodes+1):
                 else:
                     Load_Inputs2P.append(Input_itr)
             Load_Inputs2P = np.array(Load_Inputs2P,dtype=np.float32)
-            ret_action2P = Model2P(torch.from_numpy(Load_Inputs2P))
-
+            ret_action2P = Model2P(torch.from_numpy(Load_Inputs2P).to(Devise).float())
+            ret_action1P = ret_action1P.to('cpu')
+            ret_action2P = ret_action2P.to('cpu')
             action1P = ret_action1P.detach().clone().numpy()
             action2P = ret_action2P.detach().clone().numpy()
 
@@ -184,8 +186,8 @@ for episode in range(episodes+1):
             torch.save(Model1P.state_dict(),"Model_EZero/Model1P.pth")
             torch.save(Model2P.state_dict(),"Model_EZero/Model2P.pth")
             #モデルをOnnx型式でも保存する(Unityなどから呼び出せるようにするため)
-            # torch.onnx.export(Model1P,torch.from_numpy(Load_Inputs1P),"Model_EZero/Model1P.onnx")
-            # torch.onnx.export(Model2P,torch.from_numpy(Load_Inputs2P),"Model_EZero/Model2P.onnx")
+            torch.onnx.export(Model1P,torch.from_numpy(Load_Inputs1P).to(Devise).float(),"Model_EZero/Model1P.onnx")
+            torch.onnx.export(Model2P,torch.from_numpy(Load_Inputs2P).to(Devise).float(),"Model_EZero/Model2P.onnx")
             exit()
 
         #各エージェントごとのBatchedStepResultを取得
@@ -203,36 +205,16 @@ for episode in range(episodes+1):
             Experience1P = []
             Experience2P = []
             Reward1P = [float(Reward1P)]
-            Experience1P.extend(State1P)
-            Experience1P.extend(action1P)
+            Experience1P.extend(State1P[0].tolist())
+            Experience1P.extend(action1P.tolist())
             Experience1P.extend(Reward1P)
-            Experience1P.extend(NextState1P)
+            Experience1P.extend(NextState1P[0].tolist())
             #2Pも同様に格納
             Reward2P = [float(Reward2P)]
-            Experience2P.extend(State2P)
-            Experience2P.extend(action2P)
+            Experience2P.extend(State2P[0].tolist())
+            Experience2P.extend(action2P.tolist())
             Experience2P.extend(Reward2P)
-            Experience2P.extend(NextState2P)
-            # Experience1P_tmp = []
-            # Experience2P_tmp = []
-            # for Experience in Experience1P:
-            #     if type(Experience) is np.ndarray:
-            #         Experience1P_tmp.extend(Experience.tolist())
-            #     else:
-            #         Experience1P_tmp.append(float(Experience))
-            # for Experience in Experience2P:
-            #     if type(Experience) is np.ndarray:
-            #         Experience2P_tmp.extend(Experience.tolist())
-            #     else:
-            #         Experience2P_tmp.append(float(Experience))
-            
-            # Memory1P = {"Memory": Experience1P_tmp}
-            # Memory2P = {"Memory": Experience2P_tmp}
-            # with open("Model_EZero/Model1PMemory.json", "a") as f:
-            #     json.dump(Memory1P, f)
-
-            # with open("Model_EZero/Model2PMemory.json", "a") as f:
-            #     json.dump(Memory2P, f)
+            Experience2P.extend(NextState2P[0].tolist())
             Memory_1P.load(Experience1P)
             Memory_2P.load(Experience2P)
             pass
@@ -251,12 +233,12 @@ for episode in range(episodes+1):
                 else:
                     Load_Inputs1P.append(Input_itr)
             Load_Inputs1P = np.array(Load_Inputs1P,dtype=np.float32)
-            Output_Train = Model1P(torch.from_numpy(Load_Inputs1P))
-            Output_Target = Model1P_Target(torch.from_numpy(Load_Inputs1P))
-            criterion(Output_Train,Output_Target)
-            optimizer.zero_grad()
+            Output_Train = Model1P(torch.from_numpy(Load_Inputs1P).to(Devise).float())
+            Output_Target = Model1P_Target(torch.from_numpy(Load_Inputs1P).to(Devise).float())
+            loss1P = criterion(Output_Train,Output_Target)
+            optimizer1P.zero_grad()
             loss1P.backward(retain_graph=True)
-            optimizer.step()
+            optimizer1P.step()
         if Memory_2P.length() > batch_size:
             Ret_Inputs = Memory_2P.sample(batch_size)
             Inputs2P = []
@@ -270,12 +252,12 @@ for episode in range(episodes+1):
                 else:
                     Load_Inputs2P.append(Input_itr)
             Load_Inputs2P = np.array(Load_Inputs2P,dtype=np.float32)
-            Output_Train = Model2P(torch.from_numpy(Load_Inputs2P).to(Devise))
-            Output_Target = Model2P_Target(torch.from_numpy(Load_Inputs2P).to(Devise))
-            criterion(Output_Train,Output_Target)
-            optimizer.zero_grad()
+            Output_Train = Model2P(torch.from_numpy(Load_Inputs2P).to(Devise).float())
+            Output_Target = Model2P_Target(torch.from_numpy(Load_Inputs2P).to(Devise).float())
+            loss2P = criterion(Output_Train,Output_Target)
+            optimizer2P.zero_grad()
             loss2P.backward(retain_graph=True)
-            optimizer.step()
+            optimizer2P.step()
         #エピソード完了時
         if len(batched_step_results[0][1].max_step) > 0 and len(batched_step_results[1][1].max_step) > 0:
 
@@ -286,36 +268,15 @@ for episode in range(episodes+1):
                     #ReplayMemoryに格納(1Pから)
                     Experience1P = []
                     Experience2P = []
-                    Experience1P.extend(State1P)
+                    Experience1P.extend(State1P[0].tolist())
                     Experience1P.extend(action1P.tolist())
                     Experience1P.extend(Reward1P)
-                    Experience1P.extend(NextState1P)
+                    Experience1P.extend(NextState1P[0].tolist())
                     #2Pも同様に格納
-                    Experience2P.extend(State2P)
-                    Experience2P.extend(action2P)
+                    Experience2P.extend(State2P[0].tolist())
+                    Experience2P.extend(action2P.tolist())
                     Experience2P.extend(Reward2P)
-                    Experience2P.extend(NextState2P)
-                    # Experience1P_tmp = []
-                    # Experience2P_tmp = []
-                    # for Experience in Experience1P:
-                    #     if type(Experience) is np.ndarray:
-                    #         Experience1P_tmp.extend(Experience.tolist())
-                    #     else:
-                    #         Experience1P_tmp.append(float(Experience))
-                    # for Experience in Experience2P:
-                    #     if type(Experience) is np.ndarray:
-                    #         Experience2P_tmp.extend(Experience.tolist())
-                    #     else:
-                    #         Experience2P_tmp.append(float(Experience))
-                    # Memory1P = {"Memory": Experience1P_tmp}
-                    # Memory2P = {"Memory": Experience2P_tmp}
-                    # with open("Model_EZero/Model1PMemory.json", "a") as f:
-                    #     json.dump(Memory1P, f)
-                
-                    # with open("Model_EZero/Model2PMemory.json", "a") as f:
-                    #     json.dump(Memory2P, f)
-                    # Memory1P.clear()
-                    # Memory2P.clear()
+                    Experience2P.extend(NextState2P[0].tolist())
                     Memory_1P.load(Experience1P)
                     Memory_2P.load(Experience2P)
                
@@ -323,7 +284,7 @@ for episode in range(episodes+1):
 torch.save(Model1P.state_dict(),"Model_EZero/Model1P.pth")
 torch.save(Model2P.state_dict(),"Model_EZero/Model2P.pth")
 #モデルをOnnx型式でも保存する(Unityなどから呼び出せるようにするため)
-# torch.onnx.export(Model1P,torch.from_numpy(Load_Inputs1P).to(Devise),"Model_EZero/Model1P.onnx")
-# torch.onnx.export(Model2P,torch.from_numpy(Load_Inputs2P).to(Devise),"Model_EZero/Model2P.onnx")
+torch.onnx.export(Model1P,torch.from_numpy(Load_Inputs1P).to(Devise).float(),"Model_EZero/Model1P.onnx")
+torch.onnx.export(Model2P,torch.from_numpy(Load_Inputs2P).to(Devise).float(),"Model_EZero/Model2P.onnx")
 #環境のシャットダウン(プログラム終了)
 env.close()
